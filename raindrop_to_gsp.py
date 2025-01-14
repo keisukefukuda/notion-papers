5b6f130da6dc28081037e0ebc09cbabe09745fc1
Keisuke Fukuda <kfukuda@preferred.jp>
2025-01-15 00:11:03 +0900

    main: commit

--- raindrop_to_gsp.py
+++ raindrop_to_gsp.py
@@ -1,8 +1,11 @@
+import datetime
+import glob
+import json
 import os
 
+import gspread
 import requests
-# import gspread
-# from google.oauth2.service_account import Credentials
+from google.oauth2.credentials import Credentials
 
 # Raindrop.io API設定
 RAINDROP_API_TOKEN = (
@@ -13,65 +16,123 @@ RAINDROP_API_TOKEN = (
 RAINDROP_COLLECTION_NAME = "論文/unread"
 
 # Google Sheets設定
-# SPREADSHEET_NAME = "Raindrop Bookmarks"
+SPREADSHEET_ID = "17o8WlEqAS9QXe5U34Yux-z40uWuUlycf8d3gpuaY5D0"
+SHEET_NAME = "シート1"
 
-# # Google Sheets APIの認証
-# SERVICE_ACCOUNT_FILE = "path/to/your/service_account.json"
-# credentials = Credentials.from_service_account_file(
-#     SERVICE_ACCOUNT_FILE, scopes=["https://www.googleapis.com/auth/spreadsheets"]
-# )
-# gc = gspread.authorize(credentials)
+
+# Google Sheets APIの認証
+def get_credentials():
+    client_id = (
+        "456242472377-gh72p5ub9qfd22imb2obs66htm6e2i1d.apps.googleusercontent.com"
+    )
+
+    client_secret_file = glob.glob(
+        os.path.join(os.path.dirname(__file__), "client_secret_*.json")
+    )[0]
+    client_secret_info = json.load(open(client_secret_file, "r"))
+    client_secret = client_secret_info["installed"]["client_secret"]
+
+    oauth_info = json.load(
+        open(os.path.join(os.path.dirname(__file__), "oauth_info.json"), "r")
+    )
+
+    credentials = Credentials(
+        token=oauth_info["access_token"],
+        refresh_token=oauth_info["refresh_token"],
+        client_id=client_id,
+        client_secret=client_secret,
+        token_uri="https://oauth2.googleapis.com/token",
+        scopes=["https://www.googleapis.com/auth/spreadsheets"],
+    )
+    #
+    # credentials.refresh(Request())
+    return credentials
+
 
 # Raindrop.ioのAPIヘッダー
-headers = {"Authorization": f"Bearer {RAINDROP_API_TOKEN}"}
+raindrop_headers = {"Authorization": f"Bearer {RAINDROP_API_TOKEN}"}
 
 
 # 1. Raindrop.ioのフォルダIDを取得
 def get_collection_id():
-    url = "https://api.raindrop.io/rest/v1/collection"
-    response = requests.get(url, headers=headers)
+    url = "https://api.raindrop.io/rest/v1/collections"
+    response = requests.get(url, headers=raindrop_headers)
     response.raise_for_status()
     collections = response.json()["items"]
 
     for collection in collections:
         if collection["title"] == RAINDROP_COLLECTION_NAME:
             return collection["_id"]
-    raise ValueError(f"フォルダ '{RAINDROP_COLLECTION_NAME}' が見つかりません。")
+    else:
+        raise ValueError(f"フォルダ '{RAINDROP_COLLECTION_NAME}' が見つかりません。")
 
 
 # 2. ブックマークを取得
 def get_bookmarks(collection_id):
     url = f"https://api.raindrop.io/rest/v1/raindrops/{collection_id}"
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=raindrop_headers)
     response.raise_for_status()
     return response.json()["items"]
 
 
 # 3. Google Sheetsにデータを書き込む
-# def write_to_spreadsheet(bookmarks):
-#     try:
-#         # スプレッドシートを開くか、新規作成
-#         sheet = gc.open(SPREADSHEET_NAME).sheet1
-#     except gspread.SpreadsheetNotFound:
-#         sheet = gc.create(SPREADSHEET_NAME).sheet1
+def write_to_spreadsheet(bookmarks):
+    cred = get_credentials()
+    cli = gspread.authorize(cred)
 
-#     # ヘッダーの書き込み
-#     sheet.update("A1", [["Title", "URL", "Tags", "Created At"]])
+    sheet = cli.open_by_key(SPREADSHEET_ID)
 
-#     # ブックマークデータの書き込み
-#     rows = [
-#         [bm["title"], bm["link"], ", ".join(bm["tags"]), bm["created"]]
-#         for bm in bookmarks
-#     ]
-#     sheet.update("A2", rows)
+    tags = [
+        "Proj関連",
+        "気象",
+        "データ同化",
+        "Sim/PDE",
+        "LLM",
+    ]
+
+    # ヘッダーの書き込み
+    for i, h in enumerate(["Title", "優先度(1-5)", "URL", "Tags", "Created At"] + tags):
+        col = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[i]
+        sheet.values_update(
+            f"{col}2",
+            params={"valueInputOption": "USER_ENTERED"},
+            body={"values": [[h]]},
+        )
+
+    # データの下端を取得
+    last_row = len(sheet.sheet1.col_values(1))
+    print(f"{last_row=}")
+
+    # ブックマークデータの書き込み
+
+    new_rows_data = []
+
+    for bm in bookmarks:
+        created = datetime.datetime.strptime(bm["created"], "%Y-%m-%dT%H:%M:%S.%fZ")
+        new_rows_data.append(
+            [
+                bm["title"],
+                "3",
+                bm["link"],
+                ", ".join(bm["tags"]),
+                created.strftime("%Y-%m-%d %H:%M:%S"),
+            ]
+        )
+    start_cell = f"A{last_row + 1}"
+    print(f"{start_cell=}")
+    sheet.values_update(
+        start_cell,
+        params={"valueInputOption": "USER_ENTERED"},
+        body={"values": new_rows_data},
+    )
 
 
 # 4. Raindrop.ioのブックマークを削除
-# def delete_bookmarks(bookmarks):
-#     for bookmark in bookmarks:
-#         url = f"https://api.raindrop.io/rest/v1/raindrop/{bookmark['_id']}"
-#         response = requests.delete(url, headers=headers)
-#         response.raise_for_status()
+def delete_bookmarks(bookmarks):
+    for bookmark in bookmarks:
+        url = f"https://api.raindrop.io/rest/v1/raindrop/{bookmark['_id']}"
+        response = requests.delete(url, headers=raindrop_headers)
+        response.raise_for_status()
 
 
 # メイン処理
@@ -79,6 +140,7 @@ def main():
     try:
         # フォルダIDを取得
         collection_id = get_collection_id()
+        print(f"{collection_id=}")
 
         # ブックマークを取得
         bookmarks = get_bookmarks(collection_id)
@@ -86,16 +148,32 @@ def main():
             print("ブックマークが見つかりません。")
             return
 
+        bookmarks2 = []
+        for bm in bookmarks:
+            for site in ["arxiv.org", "nips.cc", "openreview.net"]:
+                if site in bm["link"]:
+                    bookmarks2.append(bm)
+                    break
+
+        bookmarks = bookmarks2
+
+        for bm in bookmarks:
+            print(f"{bm['title']}: {bm['link']}")
+
         # # Google Sheetsに書き込む
-        # write_to_spreadsheet(bookmarks)
-        # print(f"{len(bookmarks)} 件のブックマークをGoogle Spreadsheetに書き込みました。")
+        write_to_spreadsheet(bookmarks)
+        print(
+            f"{len(bookmarks)} 件のブックマークをGoogle Spreadsheetに書き込みました。"
+        )
 
-        # # Raindrop.ioのブックマークを削除
-        # delete_bookmarks(bookmarks)
-        # print(f"{len(bookmarks)} 件のブックマークをRaindrop.ioから削除しました。")
+        # Raindrop.ioのブックマークを削除
+        delete_bookmarks(bookmarks)
+        print(f"{len(bookmarks)} 件のブックマークをRaindrop.ioから削除しました。")
 
-    except Exception as e:
-        print(f"エラーが発生しました: {e}")
+    except Exception:
+        import traceback
+
+        traceback.print_exc()
 
 
 if __name__ == "__main__":
